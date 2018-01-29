d7113b72b2db2070bb2c01931f6099892b7b0111
Kevin Welcher <kaw2k@virginia.edu>
2018-01-29 14:57:19 -0500

    _lol_

--- src/views/overview.tsx
+++ src/views/overview.tsx
@@ -3,6 +3,7 @@ import { Player } from 'interfaces/game'
 import { Card } from 'interfaces/cards'
 import { getDeckWeight, getRoles, getNumberOfARole } from 'helpers'
 import { Tabs } from 'components/tabs'
+import { Row } from 'components/row'
 
 interface Props {
   players: Player[]
@@ -13,14 +14,14 @@ interface Props {
 export const Overview: React.SFC<Props> = ({ players, cards, reset }) => (
   <div>
     <h1>Players ({players.length}):</h1>
-    {players.map(player => <div key={player.name}>{player.name}</div>)}
+    {players.map(player => <Row key={player.name}>{player.name}</Row>)}
     <h1>
       Deck ({cards.length} / {getDeckWeight(cards)}):
     </h1>
     {getRoles(cards).map(role => (
-      <div key={role}>
+      <Row key={role}>
         {role} @ {getNumberOfARole(role, cards)}
-      </div>
+      </Row>
     ))}
 
     <Tabs center>
